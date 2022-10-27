5cf3aa79c38b9f3d2e2e2e180620836eb15b40ff
Ruver Dornelas <ruverd@gmail.com>
2022-10-27 09:15:35 -0300

    feat: deploy  storybook

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,36 @@
+import { Heading, HeadingProps } from '@ignite-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom title',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg', '2xl', '4xl', '5xl', '6xl'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<HeadingProps>;
+
+export const Primary: StoryObj<HeadingProps> = {};
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default this heading always will be a `h2` tag, but you can change it using the `as` attribute.',
+      },
+    },
+  },
+};
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,51 @@
+import { Text, TextProps } from '@ignite-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro reprehenderit est veniam? Rem recusandae sint perspiciatis debitis corrupti. Deserunt neque exercitationem illo voluptatum laudantium culpa suscipit accusantium blanditiis voluptas numquam.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default this text always will be a `p` tag, but you can change it using the `as` attribute.',
+      },
+    },
+  },
+};
